3b06cbcc3155a89f0773413f68b7889954ecdd02
ultraviper <hendrix@inbox.ru>
2021-09-29 11:41:56 +0300

    ))

---
+++ index.js
@@ -0,0 +1,8 @@
+slides = document.querySelectorAll('.slide')
+slides.forEach(slide => {
+    slide.addEventListener('click',()=>{
+        slides.forEach((slide) => slide.classList.remove('active'))
+        slide.classList.add('active')
+    })
+});
+
